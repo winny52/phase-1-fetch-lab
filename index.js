5d59b896c5572edc0b998bb03882c0e8d4ba732c
WinnySigilai <winnysigilai02@gmail.com>
2023-07-07 18:19:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+   fetch("https://anapioficeandfire.com/api/books'")
+  .then(function(res){
+    return res.json();
+  })
+  .then(data => renderBooks(data));
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
